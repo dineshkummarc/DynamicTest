2d9d4f2270f212b3797f480dcfe6a528d12b9244
eiu165 <eiu165@gmail.com>
2012-02-23 11:05:01 -0600

    use ExpandoObject();

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using ConsoleApplication1;
@@ -11,7 +12,12 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Movie m = new Movie();
-            var o = new { Title = "Ghostbusters", Rating = "PG" };
+            //var o = new { Title = "Ghostbusters", Rating = "PG" };
+
+            dynamic o = new ExpandoObject();
+            o.Title = "Ghostbusters";
+            o.Rating = "PG";
+            
             Console.WriteLine(m.PrintMovie(o));
         }
     }
